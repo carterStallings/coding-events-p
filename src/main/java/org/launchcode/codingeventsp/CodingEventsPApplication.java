851361d4caff97046b2d63aa130ba13d8c03120d
carterStallings <carterstallings1848@gmail.com>
2020-12-30 22:24:08 -0600

    Initial commit for coding-events

---
+++ src/main/java/org/launchcode/codingeventsp/CodingEventsPApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingeventsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingEventsPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingEventsPApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/codingeventsp/CodingEventsPApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingeventsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingEventsPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
